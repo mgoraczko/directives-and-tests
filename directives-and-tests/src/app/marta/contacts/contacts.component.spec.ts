cba4ca6e36ea292d1a9784a9ddbc70a0f152c883
SIIPOLSKA\mgoraczko <mgoraczko@sii.pl>
2023-11-26 17:40:01 +0100

    add ids for buttons

--- directives-and-tests/src/app/marta/contacts/contacts.component.spec.ts
+++ directives-and-tests/src/app/marta/contacts/contacts.component.spec.ts
@@ -1,13 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ContactsComponent } from './contacts.component';
 
-describe('ContactsComponent', () => {
+import { ContactsComponent } from './contacts.component';
+import { ContactService } from '../services/contact.service';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+fdescribe('ContactsComponent', () => {
   let component: ContactsComponent;
   let fixture: ComponentFixture<ContactsComponent>;
+  let contactServiceMock: ContactService;
+  const contacts = [{
+    name: 'Marta',
+    surname: 'Goraczko',
+    email: 'tenMinuteMail@gmail.com',
+    phoneNumber: '+48500600700'
+  }];
+
+  contactServiceMock = {
+    getContacts: jasmine.createSpy('getContacts').and.returnValue(of(contacts))
+  } as jasmine.SpyObj<ContactService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContactsComponent ]
+      declarations: [ ContactsComponent ],
+      providers: [
+        { provide: ContactService, useValue: contactServiceMock }
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
     })
     .compileComponents();
 
@@ -16,7 +36,38 @@ describe('ContactsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should fill in the behaviour subject with data from contact service and put that into html template', () => {
+
+    const elementsToFulfill = fixture.debugElement.queryAll(By.css('tr'))[contacts.length].childNodes;
+
+    expect(component.getContacts.value).toEqual(contacts);
+    expect(elementsToFulfill[0].nativeNode.innerText).toEqual(contacts[0].name);
+    expect(elementsToFulfill[1].nativeNode.innerText).toEqual(contacts[0].surname);
+    expect(elementsToFulfill[2].nativeNode.innerText).toEqual(contacts[0].email);
+    expect(elementsToFulfill[3].nativeNode.innerText).toEqual(contacts[0].phoneNumber);
+  });
+
+  it('when clicking on addNewContact button it should be disabled and hideNewContactForm button should appear', () => {
+    const addNewContactButton = fixture.debugElement.query(By.css('#addNewContactBtn')).nativeElement;
+
+    addNewContactButton.click();
+    fixture.detectChanges();
+    const hideFormBtn = fixture.debugElement.query(By.css('#hideNewContactFormBtn')).nativeElement;
+
+    expect(hideFormBtn).toBeTruthy();
+    expect(addNewContactButton.disabled).toBe(true);
+  });
+
+  it('when clicking on hideNewContactForm button addNewContact button should be active', () => {
+    const addNewContactButton = fixture.debugElement.query(By.css('#addNewContactBtn')).nativeElement;
+    addNewContactButton.click();
+    fixture.detectChanges();
+
+    const hideFormBtn = fixture.debugElement.query(By.css('#hideNewContactFormBtn')).nativeElement;
+    hideFormBtn.click();
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.query(By.css('#hideNewContactFormBtn'))).toBeNull();
+    expect(addNewContactButton.disabled).toBe(false);
   });
 });
